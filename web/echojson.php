d5329142b117346aa7c65fc0954080009b7101ae
Federico Garcea <v-fedgar@microsoft.com>
2021-02-15 22:15:58 +0100

    Add javascript frontend, and read json sent from it in php.

--- web/echojson.php
+++ web/echojson.php
@@ -1,8 +1,30 @@
 <?php
-echo "Hello";
+echo "Hello<br>";
 
 $json = file_get_contents('php://input');
 $obj = json_decode($json);
 
-print_r($obj);
+echo $obj->id;
+echo "<br>";
+
+//echo print_r($obj->resp, true);
+
+$i = 0;
+foreach($obj->resp as $individual_response){
+    $best = $individual_response->best;
+	$worst = $individual_response->worst;
+	echo "sentence: ",$i,"<br>";
+	echo "best: ",$best,"<br>";
+	echo "worst: ",$worst,"<br>";
+	$i++;
+}
+
+//echo print_r($obj);
+
+//echo $_POST["id"];
+//$obj = json_decode($_POST, true);
+//echo print_r($obj,true);
+
+//echo print_r($_POST);
+
 ?>
